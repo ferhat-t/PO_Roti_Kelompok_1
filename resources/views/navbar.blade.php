76014fd32c653cb0b2e26f0ee16286251c47113d
diiivp <sryoooadii@gmail.com>
2025-12-11 16:06:23 +0700

    commit kedua

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ route('location.index') }}">
+    <button class="px-4 py-2 rounded-lg border border-gray-400 hover:bg-gray-100">
+        Location <span>📍</span>
+    </button>
+</a>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
 
 Route::get('/', function () {
     return view('welcome');
+
+
 });
+
+Route::get('/location', [LocationController::class, 'index'])->name('location.index');
